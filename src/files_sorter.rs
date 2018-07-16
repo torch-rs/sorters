4a1d063aa4d7523e11441f072148ddd94c30f7f4
Patrick Chan <patrickchan1999@gmail.com>
2018-07-16 16:18:42 -0400

    Add a files sorter that prioritize non-hidden files over hidden

---
+++ src/files_sorter.rs
@@ -0,0 +1,48 @@
+extern crate regex;
+
+use std::cmp::Ordering;
+use std::path::MAIN_SEPARATOR;
+
+use self::regex::Regex;
+
+use Sorter;
+
+pub struct FilesSorter;
+
+impl Sorter for FilesSorter {
+
+    fn sort(candidates: &Vec<String>) -> Vec<String> {
+        let mut candidates = candidates.to_vec();
+        candidates.sort_by(|a, b| {
+            let hidden_file_pattern = Regex::new(&regex::escape(&format!("{}.", MAIN_SEPARATOR))).unwrap();
+            if hidden_file_pattern.is_match(a) && hidden_file_pattern.is_match(b) {
+                return a.cmp(b);
+            } else if hidden_file_pattern.is_match(a) {
+                return Ordering::Greater;
+            }else if hidden_file_pattern.is_match(b) {
+                return Ordering::Less;
+            } else {
+                return a.cmp(b);
+            }
+        });
+        candidates
+    }    
+    
+}
+
+#[cfg(test)]
+mod tests {
+
+    use Sorter;
+    use files_sorter::FilesSorter;
+
+    #[test]
+    fn test_sort() {
+        let test_vec = vec!["/home/test/nothidden".to_string(), "/home/test/.hidden".to_string(),
+                            "/home/test/.hiddentwo".to_string(), "/home/test/.hiddenthree".to_string(),
+                            "/home/test/nothiddentwo".to_string()];
+        assert_eq!(FilesSorter::sort(&test_vec), ["/home/test/nothidden", "/home/test/nothiddentwo",
+                                                  "/home/test/.hidden", "/home/test/.hiddenthree",
+                                                  "/home/test/.hiddentwo"]);
+    }
+}
