3d95cb80c8d44f8b6af58aadf3782aaba7cb2837
Patrick Chan <patrickchan1999@gmail.com>
2018-07-18 22:35:37 -0400

    Add WeightedSort trait - All sorting algorithms that sort based on some weight should implement this trait

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,17 @@ pub trait Sort {
 
 }
 
+pub trait WeightedSort {
+
+    fn decrement_weight(&mut self, key: String);
+    fn increment_weight(&mut self, key: String);
+    fn new(filename: String) -> Self;
+    fn save(&self);
+    fn sort(&self, candidates: &Vec<String>) -> Vec<String> ;
+    fn update_weight(&mut self, key: String, value: i32);
+
+}
+
 #[cfg(test)]
 mod tests {
 
